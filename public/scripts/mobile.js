973accb810844313676e1c99a585fe1f83d0d28f
bikash ghimire <bikashghimire@mogroup.com>
2023-07-11 21:37:12 +0300

    toggling the mobile menu

---
+++ public/scripts/mobile.js
@@ -0,0 +1,8 @@
+const mobileMenuBtnElement = document.getElementById("mobile-menu-btn");
+const mobileMenuElement = document.getElementById("mobile-menu");
+
+function toggleMobileMenu() {
+  mobileMenuBtnElement.classList.toggle("open");
+}
+
+mobileMenuBtnElement.addEventListener("click", toggleMobileMenu);
